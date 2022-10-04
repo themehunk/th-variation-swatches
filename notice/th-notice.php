5bc0cfec53d7462cac759a34cb2357a80a021352
shivam kushwah <kushwah.shivam30@gmail.com>
2022-10-04 13:50:01 +0530

    save it

--- notice/th-notice.php
+++ notice/th-notice.php
@@ -35,10 +35,19 @@ class TH_Variation_Swatches_Notice{
     } 
 
     function th_variation_swatches_admin_notice() { 
-      $display = isset($_GET['ntc-vrtn-disable'])?'none':'block';
+
+      if(isset($_GET['ntc-vrtn-disable'])){
+
+          $display ='none';
+
+          }else{
+
+          $display ='block';
+
+          }
     ?>
 
-    <div class="th-variation-swatches-notice notice" style="display:<?php echo $display; ?>;">
+    <div class="th-variation-swatches-notice notice" style="display:<?php echo esc_attr($display); ?>;">
         <div class="th-variation-swatches-notice-wrap">
             <div class="th-variation-swatches-notice-image"><img src="<?php echo esc_url( TH_VARIATION_SWATCHES_PLUGIN_URI.'notice/img/swatches-pro.png');?>" alt="<?php _e('TH Variation Swatches Pro','th-variation-swatches'); ?>"></div>
             <div class="th-variation-swatches-notice-content-wrap">
@@ -72,7 +81,8 @@ class TH_Variation_Swatches_Notice{
     function th_variation_swatches_notice_unset_cookie(){
 
             $visit_time = time();
-            $cookie_time = $_COOKIE['thntc_vrtn_time'];
+
+            $cookie_time = isset($_COOKIE['thntc_vrtn_time']) ? sanitize_key($_COOKIE['thntc_vrtn_time']) : '0';
 
             if ($cookie_time < $visit_time) {
                 setcookie('thntc_vrtn_time', null, strtotime('-1 day'));
