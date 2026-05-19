17ef12fd411a77076fa6012d608c4d7bf160fb91
iheeresh <heereshd@gmail.com>
2026-05-19 15:49:36 +0530

    style update

--- inc/thvs-front-custom-style.php
+++ inc/thvs-front-custom-style.php
@@ -5,15 +5,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function thvs_front_custom_style($thvs_frnt_custom_css=''){
 if(wc_string_to_bool( th_variation_swatches()->th_variation_swatches_get_option( 'tooltip' ) )):	
 $thvs_frnt_custom_css.="[data-thvstooltip]:before {
-  min-width: 100px;
+  width: max-content;
   content: attr(data-thvstooltip);
 }
 [data-thvstooltip]:before {
     margin-bottom: 5px;
     -webkit-transform: translateX(-50%);
     transform: translateX(-50%);
-    padding: 7px;
-    border-radius: 3px;
+    padding: 5px 12px;
+    border-radius: 5px;
     background-color: #111;
     background-color: #111;
     color: #fff;
@@ -90,7 +90,8 @@ $attr_brdr_hvr_color          = esc_html(th_variation_swatches()->th_variation_s
 $attr_text_hvr_color          = esc_html(th_variation_swatches()->th_variation_swatches_get_option( 'attr_text_hvr_color' ));
 $attr_bg_btn_hvr_color        = esc_html(th_variation_swatches()->th_variation_swatches_get_option( 'attr_bg_btn_hvr_color' ));
 
-$thvs_frnt_custom_css.=".th-variation-swatches .variable-items-wrapper .variable-item:hover,.th-variation-swatches .variable-items-wrapper .variable-item.selected{border:{$attr_brdr_size}px solid {$attr_brdr_hvr_color };} .th-variation-swatches .variable-items-wrapper .variable-item:hover,.th-variation-swatches .variable-items-wrapper .variable-item.selected{background-color:{$attr_bg_btn_hvr_color}!important;color:{$attr_text_hvr_color};box-shadow:none;} ";
+$thvs_frnt_custom_css.=".th-variation-swatches .variable-items-wrapper .variable-item:hover,.th-variation-swatches .variable-items-wrapper .variable-item.selected{border:{$attr_brdr_size}px solid {$attr_brdr_hvr_color };} .th-variation-swatches .variable-items-wrapper .variable-item.selected{background-color:{$attr_bg_btn_hvr_color}!important;color:{$attr_text_hvr_color};box-shadow:none;}
+.th-variation-swatches .variable-items-wrapper .variable-item:hover{border-color:{$attr_bg_btn_hvr_color};} ";
 $attr_title_font_size        = esc_html(th_variation_swatches()->th_variation_swatches_get_option( 'attr_title_font_size' ));
 $thvs_frnt_custom_css.=".variations .woo-selected-variation-item-name,.variations .label label{
 font-size:{$attr_title_font_size}px;
